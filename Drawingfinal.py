39df02a7ed9ccdec96a2b6af582601e3f3e77595
willj2003 <willstadler@live.com>
2017-08-10 19:31:18 -0400

    Add files via upload

---
+++ Drawingfinal.py
@@ -0,0 +1,70 @@
+import pygame, random
+
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+
+done = False
+draw_on = False
+last_pos = (0, 0)
+color = (255,128, 0)
+radius = 3 
+r = random.randint(20,255)
+g = random.randint(20,255)
+b = random.randint(20,255)
+
+def roundline(srf, color,start, end, radius=1):
+    dx = end[0]-start[0]
+    dy = end[1]-start[1]
+    distance = max(abs(dx), abs(dy))
+    for i in range(distance):
+        x = int( start[0]+float(i)/distance*dx)
+        y = int( start[1]+float(i)/distance*dy)
+        pygame.draw.circle(srf, color, (x, y), radius)
+
+while not done:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            done = True
+            pygame.quit()
+            break
+
+    if done: break
+
+    screen.fill([0, 0, 0])
+
+  
+    e = pygame.event.wait()
+    
+    if e.type == pygame.QUIT:
+        done= True
+        break
+    
+    if e.type == pygame.MOUSEBUTTONDOWN:
+        print(e.button)
+        if e.button == 1:
+            print("drawing")
+            color = (r, g, b)
+            pygame.draw.circle(screen, color, e.pos, radius)
+            draw_on =True
+            print (draw_on)
+        elif e.button == 3:
+            print("changing")
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0 ,255)
+            #print ("r" + r + "g" + g + "b" + b)
+    if e.type == pygame.MOUSEBUTTONUP:
+        draw_on = False 
+    if e.type == pygame.MOUSEMOTION:
+        if draw_on:
+            print (
+            pygame.draw.circle(screen, color, e.pos, radius)
+            #roundline(screen,color, e.pos, last_pos, radius)
+        last_pos = e.pos
+
+    pygame.display.flip()
+
+
+#pygame.quit()
+
+            
